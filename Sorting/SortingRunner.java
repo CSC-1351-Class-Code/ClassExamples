909357df01aef7da8b67dbf84f511f790493e5ae
John Luke <jldenny0207@aol.com>
2026-04-12 19:01:09 -0500

    removed unused function

--- Sorting/SortingRunner.java
+++ Sorting/SortingRunner.java
@@ -341,24 +341,6 @@ public class SortingRunner {
         return i + 1;
     }
 
-    /* The main function that implements QuickSort()
-    arr[] --> Array to be sorted,
-    low --> Starting index,
-    high --> Ending index */
-    static void qSort(int arr[], int low, int high)
-    {
-        if (low < high) {
-            /* pi is partitioning index, arr[pi] is
-            now at right place */
-            int pi = partition(arr, low, high);
-
-            // Recursively sort elements before
-            // partition and after partition
-            qSort(arr, low, pi - 1);
-            qSort(arr, pi + 1, high);
-        }
-    }
-
 	public static void quickSortIter(int[] arr){
 		Stack<int[]> stack = new Stack();
 		int l,h;
